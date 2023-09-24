b43bf6d216b1d798ab27648b3ead741b5709a27d
preityzz <106267402+preityzz@users.noreply.github.com>
2023-09-24 14:57:55 +0530

    added some components

---
+++ client/src/components/home/hero/Hero.jsx
@@ -0,0 +1,29 @@
+import React from "react"
+import Heading from "../../common/heading/Heading"
+import "./Hero.css"
+
+const Hero = () => {
+  return (
+    <>
+      <section className='hero'>
+        <div className='container'>
+          <div className='row'>
+            <Heading subtitle='WELCOME TO E-WASTELOCATOR' title='Best Online Education Expertise' />
+            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+            <div className='button'>
+              <button className='primary-btn'>
+                LOGIN<i className='fa fa-long-arrow-alt-right'></i>
+              </button>
+              <button>
+                SIGN UP <i className='fa fa-long-arrow-alt-right'></i>
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+      <div className='margin'></div>
+    </>
+  )
+}
+
+export default Hero
